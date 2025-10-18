b9ff11e7b1248a168dbd1389100e87a5c299ba09
Miaoyuanyuan777 <miaoyuanyuan2022@sinh.ac.cn>
2025-10-19 00:47:06 +0800

    box_plot

--- box_plot/fig2/intra_method_auc_box.R
+++ box_plot/fig2/intra_method_auc_box.R
@@ -4,34 +4,34 @@ library(dplyr)
 library(tidyr)
 df <- read_csv('intra_method_auc.csv')
 
-# 将 Metric 转换为因子并指定顺序
+# Convert Metric & Method & Dataset to factor and specify the order
 df$Metric <- factor(df$Metric, levels = c("auROC", "per cell auROC", "per peak auROC", 
                                           "auPRC", "per cell auPRC", "per peak auPRC"))
 df$Method <- factor(df$Method, levels = c("Seurat", "LS_Lab", "MultiVI", "XChrom"))
 df$Dataset<-factor(df$Dataset, levels = c("h_pbmc","m_brain","h_brain",'h_gonads','m_palates'))
 
-# 添加一个新的列来标识 ns 和 ls
+# Add 2 new column to group auROC(overall,per-cell,per-peak) and auPRC(overall,per-cell,per-peak),respectively
 df <- df %>%
   mutate(MetricType = ifelse(grepl("auPRC", Metric), "auPRC", "auROC"))%>%
   mutate(MetricType = factor(MetricType, levels = c("auROC", "auPRC")))
 
-# 获取所有需要比较的方法（排除XChrom自身）
+# Obtain all the methods that need to be compared (excluding XChrom itself)
 other_methods <- unique(df$Method[df$Method != "XChrom"])
 
-# 提取XChrom的数据作为基准
+# Extract the data of XChrom as the benchmark
 xchrom_data <- df %>% 
   filter(Method == "XChrom") %>% 
   select(Dataset, Metric, MetricType, Value_XChrom = Value)
 
-# 生成比较数据
+# Generate comparative data
 comparison_data <- df %>%
   filter(Method != "XChrom") %>% 
   inner_join(xchrom_data, by = c("Dataset", "Metric", "MetricType")) %>%
   mutate(diff = Value_XChrom - Value)
 
-# 计算统计指标
+# Calculate statistical results
 annotation_df <- comparison_data %>%
-  group_by(Metric, Method) %>%  # 按Metric和对比方法分组
+  group_by(Metric, Method) %>%  # # Group by Metric & Test
   summarise(
     mean_diff = mean(diff, na.rm = TRUE),
     se_diff = sd(diff, na.rm = TRUE) / sqrt(n()),
@@ -45,7 +45,7 @@ annotation_df <- comparison_data %>%
                            paste0("p = ", signif(p_value, digits = 3)),
                            NA_character_)
   ) %>%
-  # 合并MetricType和y轴位置信息
+  # Merge the MetricType and Y-axis position information
   left_join(distinct(df, Metric, MetricType), by = "Metric") %>% 
   left_join(
     df %>% group_by(Metric) %>% summarise(max_val = max(Value)),
@@ -53,28 +53,27 @@ annotation_df <- comparison_data %>%
   ) %>% 
   mutate(y_position = ifelse(p_value < 0.05, max_val + 0.15, NA_real_))
 
-# 绘制分面图，使用不同的纵坐标尺度并去除没有数据的分面轴
 ggplot(df, aes(x = Metric, y = Value,)) +
   geom_boxplot(aes(group = interaction(Metric, Method), fill = Method), 
-               alpha = 1, outlier.shape = NA) +  # 绘制箱线图
+               alpha = 1, outlier.shape = NA) +  
   geom_jitter(
     aes(group = interaction(Metric, Method), shape = Dataset),
     position = position_jitterdodge(
-      jitter.width = 0.2,   # 控制抖动幅度
-      dodge.width = 0.8     # 控制不同 Scenario 的间距
+      jitter.width = 0.2,   
+      dodge.width = 0.8    
     ),size = 3,alpha = 1,color = "black",#stroke = 0.8
     )+
-  scale_fill_manual(values = c("Seurat" = "skyblue", 'LS_Lab' = "pink",'MultiVI'='#CC99E6',"XChrom" = "#FFB07C")) +  # 自定义箱线图颜色
+  scale_fill_manual(values = c("Seurat" = "skyblue", 'LS_Lab' = "pink",'MultiVI'='#CC99E6',"XChrom" = "#FFB07C")) + 
   scale_shape_manual(
     values = c(
-      "h_pbmc" = 1,    # 空心圆
-      "m_brain" = 2,    # 空心三角
-      "h_brain" = 5,    # 空心菱形
-      "h_gonads" = 0,    # 空心方框
-      "m_palates" = 6    # 空心倒三角
+      "h_pbmc" = 1,   
+      "m_brain" = 2,    
+      "h_brain" = 5,    
+      "h_gonads" = 0,   
+      "m_palates" = 6    
     ))+
-  scale_x_discrete(expand = expansion(add = c(0.5, 0.5))) +  # 增加 x 轴 Metric 之间的间距
-  guides(shape = guide_legend(order = 1), fill = guide_legend(order = 2)) +  # 图例顺序
+  scale_x_discrete(expand = expansion(add = c(0.5, 0.5))) +  
+  guides(shape = guide_legend(order = 1), fill = guide_legend(order = 2)) +  
   facet_wrap(~MetricType, scales = "free")+
   theme_set(theme_bw())+
   # theme_minimal() +
@@ -85,14 +84,14 @@ ggplot(df, aes(x = Metric, y = Value,)) +
     legend.title = element_blank(),
     plot.title = element_text(hjust = 0.5, size = 20, face = "bold",margin = margin(b = 10)),
     legend.text = element_text(size = 16),
-    strip.background = element_rect(fill = rgb(0.5, 0.5, 0.5, 0.3),color = "black", size = 0.5),  # 设置分面标签背景
-    strip.text = element_text(size = 16, face = "bold", color = "black"),  # 设置分面标签的文字样式
-    strip.text.x = element_text(size = 16, face = "bold",color = "black"),  # 设置分面标签的文本样式
+    strip.background = element_rect(fill = rgb(0.5, 0.5, 0.5, 0.3),color = "black", size = 0.5),  
+    strip.text = element_text(size = 16, face = "bold", color = "black"),  
+    strip.text.x = element_text(size = 16, face = "bold",color = "black"), 
   ) +
   labs(title = "Cross-cell Prediction", x = "", y = "Value") +
   geom_text(
     data = annotation_df[annotation_df$p_value<0.05,],
-    aes(x = Metric, y = y_position,group = MetricType,  # x=1.5 将注释居中于分面面板
+    aes(x = Metric, y = y_position,group = MetricType,  
         label = paste0(p_value_label, "\n95%CI = [",
                        round(lower_ci, 2), ",",
                        round(upper_ci, 2), "]")),
@@ -100,5 +99,5 @@ ggplot(df, aes(x = Metric, y = Value,)) +
     color = "black",hjust = 0.5)
 
 
-ggsave("intra_method_auc_box1.pdf", plot = last_plot(), 
+ggsave("intra_method_auc_box.pdf", plot = last_plot(), 
        width = 20, height = 6, units = "in",dpi=300)
